0431e13a4da765c2eacd01205114d90b596c0424
TwentyFive <TwentyFiveSoftware@gmail.com>
2022-05-27 23:14:52 +0200

    random scene generation

--- src/scene.cpp
+++ src/scene.cpp
@@ -1,4 +1,5 @@
 #include "scene.h"
+#include "utils.h"
 
 Scene::Scene() : spheres() {
     // ground
@@ -6,9 +7,29 @@ Scene::Scene() : spheres() {
                              {MaterialType::DIFFUSE, TextureType::CHECKERED, glm::vec3(0.05f, 0.05f, 0.05f),
                               glm::vec3(0.95f, 0.95f, 0.95f)}));
 
+    for (int x = -11; x < 11; ++x) {
+        for (int z = -11; z < 11; ++z) {
+            float materialRandom = randomFloat();
+
+            Material material = {};
+
+            if (materialRandom < 0.8f) {
+                material = Material{.type = MaterialType::DIFFUSE, .albedo = getRandomColor()};
+            } else if (materialRandom < 0.95f) {
+                material = Material{.type = MaterialType::METAL, .albedo = getRandomColor()};
+            } else {
+                material = Material{.type = MaterialType::DIELECTRIC, .refractionIndex = 1.5f};
+            }
+
+            spheres.emplace_back(glm::vec3(float(x) + 0.9f * randomFloat(), 0.2f, float(z) + 0.9 * randomFloat()),
+                                 0.2f, material);
+        }
+    }
+
+
     // left
     spheres.push_back(Sphere(glm::vec3(-4.0f, 1.0f, 0.0), 1.0f,
-                             {MaterialType::DIFFUSE, TextureType::SOLID,glm::vec3(0.6f, 0.3f, 0.1f)}));
+                             {MaterialType::DIFFUSE, TextureType::SOLID, glm::vec3(0.6f, 0.3f, 0.1f)}));
 
     // center
     spheres.push_back(Sphere(glm::vec3(0.0f, 1.0f, 0.0), 1.0f,
@@ -16,7 +37,7 @@ Scene::Scene() : spheres() {
 
     // right
     spheres.push_back(Sphere(glm::vec3(4.0f, 1.0f, 0.0), 1.0f,
-                             {MaterialType::METAL, TextureType::SOLID,glm::vec3(0.7f, 0.6f, 0.5f)}));
+                             {MaterialType::METAL, TextureType::SOLID, glm::vec3(0.7f, 0.6f, 0.5f)}));
 }
 
 std::vector<Sphere> Scene::getSpheres() const {
--- src/utils.h
+++ src/utils.h
@@ -10,7 +10,8 @@ glm::vec3 getRandomUnitVector();
 
 bool isNearZero(const glm::vec3 &vector);
 
-
 glm::vec3 reflect(const glm::vec3 &vector, const glm::vec3 &normal);
 
 glm::vec3 refract(const glm::vec3 &vector, const glm::vec3 &normal, float refractionRatio);
+
+glm::vec3 getRandomColor();
